0461ee387e5f9843131e61cbebbf4a5ea7e7fe81
sanika2001 <porayilsanika@gmail.com>
2020-08-04 22:44:38 +0530

    restaurant screen

--- lib/Repository/Order.dart
+++ lib/Repository/Order.dart
@@ -6,15 +6,15 @@ List<Order> orderList = [
       "Taj Restaurant",
       "11 June 2020  03:23"),
   Order(
-      "https://media-cdn.tripadvisor.com/media/photo-s/05/2a/0d/f2/taj-mahal.jpg",
+      "https://media-cdn.tripadvisor.com/media/photo-s/0a/7b/2e/c1/restaurant-kfc.jpg",
       "KFC Kannur",
       "08 June 2020  03:23"),
   Order(
-      "https://media-cdn.tripadvisor.com/media/photo-s/05/2a/0d/f2/taj-mahal.jpg",
+      "https://i.pinimg.com/originals/e7/d4/09/e7d409ebd4a2ec00a119d08ab81107bd.jpg",
       "Greens Supermarket",
       "07 June 2020  03:23"),
   Order(
-      "https://media-cdn.tripadvisor.com/media/photo-s/05/2a/0d/f2/taj-mahal.jpg",
+      "https://www.kenresearch.com/blog/wp-content/uploads/2018/06/India-Hypermarkets-Research-Report.jpg",
       "METROHOME Hypermarket",
       "17 June 2020  03:23"),
 ];
