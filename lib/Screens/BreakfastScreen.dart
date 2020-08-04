99362e62cb37593247728d37824ab93192bce804
sanika2001 <porayilsanika@gmail.com>
2020-08-04 18:20:35 +0530

    breakfast screen

---
+++ lib/Screens/BreakfastScreen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
+class BreakfastScreen extends StatefulWidget {
+  @override
+  _BreakfastScreenState createState() => _BreakfastScreenState();
+}
+
+class _BreakfastScreenState extends State<BreakfastScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF87ceeb),
+      ),
+    );
+  }
+}
