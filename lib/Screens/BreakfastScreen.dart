daed74d012e39fa1acbfdd06c353d8c0a785438a
sanika2001 <porayilsanika@gmail.com>
2020-08-04 20:48:49 +0530

    breakfast screen

--- lib/Screens/BreakfastScreen.dart
+++ lib/Screens/BreakfastScreen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:feelsgood/Components/BottomBar.dart';
+import 'package:feelsgood/Components/SwiperCard.dart';
 
 class BreakfastScreen extends StatefulWidget {
   static const String id = '/Breakfast';
@@ -11,6 +13,7 @@ class _BreakfastScreenState extends State<BreakfastScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: BottomBar(),
       appBar: AppBar(
         backgroundColor: Color(0xFF87ceeb),
         elevation: 0,
@@ -29,14 +32,25 @@ class _BreakfastScreenState extends State<BreakfastScreen> {
                   topRight: Radius.circular(41), topLeft: Radius.circular(41)),
             ),
           ),
-          Swiper(
-            itemBuilder: (BuildContext context, int index) {
-              return Container();
-            },
-            autoplay: false,
-            itemCount: 3,
-            pagination: SwiperPagination(),
-            control: SwiperControl(),
+          Positioned(
+            top: 85,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: Center(
+                child: Swiper(
+                  itemBuilder: (BuildContext context, int index) {
+                    return SwiperCard(
+                      index: index,
+                    );
+                  },
+                  autoplay: false,
+                  itemCount: 3,
+                  pagination: SwiperPagination(),
+                  control: SwiperControl(),
+                ),
+              ),
+            ),
           ),
         ],
       ),
