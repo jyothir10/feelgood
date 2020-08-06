4966a78b6fa960eead8c797ddbdb1d9d440696e9
sanika2001 <porayilsanika@gmail.com>
2020-08-06 13:23:11 +0530

    fruits screen

--- lib/Screens/FruitScreen.dart
+++ lib/Screens/FruitScreen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class FruitScreen extends StatefulWidget {
   static const String id = '/Fruits';
@@ -21,28 +22,80 @@ class _FruitScreenState extends State<FruitScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Card(
-                elevation: 40,
-                child: Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Image(image: AssetImage("images/fruit.png"),
-                  height: 50,),
+              Container(
+                height: 155,
+                width: 140,
+                child: Card(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20)),
+                  color: Color(0xFFACB6B9),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: <Widget>[
+                      Container(
+                        height: 140,
+                        width: 125,
+                        decoration: BoxDecoration(
+                          color: Color(0xFFECECEC),
+                          borderRadius: BorderRadius.only(
+                              topLeft: Radius.circular(20),
+                              topRight: Radius.circular(15),
+                              bottomLeft: Radius.circular(15),
+                              bottomRight: Radius.circular(20)),
+                        ),
+                        child: Column(
+                          children: <Widget>[
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 4, right: 4),
+                                  child: CircleAvatar(
+                                    radius: 14,
+                                    backgroundColor: Colors.white,
+                                    child: Icon(
+                                      FontAwesomeIcons.heart,
+                                      color: Colors.black,
+                                      size: 14,
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                            Image(
+                              height: 90,
+                              image: AssetImage("images/avocado.png"),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 7),
-                child: Text("Avocado",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 15,
-                ),),
+                child: Text(
+                  "Avocado",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w900,
+                    fontFamily: "Open Sans",
+                    fontSize: 19,
+                  ),
+                ),
               ),
               Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 7),
-                child: Text("₹ 80",
+                child: Text(
+                  "\$1",
                   style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),),
+                    fontWeight: FontWeight.w900,
+                    fontFamily: "Open Sans",
+                    fontSize: 19,
+                  ),
+                ),
               ),
             ],
           ),
