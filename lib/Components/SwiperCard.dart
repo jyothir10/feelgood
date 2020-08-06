eebcc9cb706f1946267c21592ff709b16511017c
sanika2001 <porayilsanika@gmail.com>
2020-08-06 14:13:22 +0530

    breakfast screen

--- lib/Components/SwiperCard.dart
+++ lib/Components/SwiperCard.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:feelsgood/bloc/Cuisinebloc.dart';
+import 'package:feelsgood/Models/Cuisine.dart';
+import 'package:feelsgood/Components/CuisineCard.dart';
 
 class SwiperCard extends StatelessWidget {
+  CuisineBloc _cuisineBloc = CuisineBloc();
+
   int index;
   Color color;
+  String text;
 
   SwiperCard({this.index});
 
@@ -10,10 +16,13 @@ class SwiperCard extends StatelessWidget {
   Widget build(BuildContext context) {
     if (index == 0) {
       color = Color(0xFF96CA6D);
+      text = "BreakFast";
     } else if (index == 1) {
       color = Color(0xFF87ceeb);
+      text = "Lunch";
     } else if (index == 2) {
       color = Color(0xFFFFD782);
+      text = "Dinner";
     }
 
     return Container(
@@ -22,7 +31,52 @@ class SwiperCard extends StatelessWidget {
         borderRadius: BorderRadius.all(Radius.circular(30)),
       ),
       child: Column(
-        children: <Widget>[],
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: Text(
+              text,
+              style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w800,
+                  fontSize: 30),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 25),
+            child: SingleChildScrollView(
+              child: Container(
+                height: 500,
+                width: 380,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 8),
+                  child: StreamBuilder<List<Cuisine>>(
+                    stream: _cuisineBloc.cuisineListStream,
+                    builder: (BuildContext context,
+                        AsyncSnapshot<List<Cuisine>> snapshot) {
+                      if (snapshot.hasError) {
+                        print(snapshot.error);
+                      }
+                      return snapshot.hasData
+                          ? ListView.builder(
+                              shrinkWrap: true,
+                              scrollDirection: Axis.vertical,
+                              itemCount: snapshot.data.length,
+                              itemBuilder: (context, index) {
+                                return CuisineCard(
+                                  cuisine: snapshot.data[index],
+                                );
+                              })
+                          : Center(
+                              child: CircularProgressIndicator(),
+                            );
+                    },
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
