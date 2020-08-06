5fd88dd4789c4125011704c4f759cef123e78ddb
sanika2001 <porayilsanika@gmail.com>
2020-08-06 13:41:22 +0530

    cuisine screen

--- lib/Components/CuisineCard.dart
+++ lib/Components/CuisineCard.dart
@@ -13,7 +13,7 @@ class CuisineCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(8.0),
+      padding: const EdgeInsets.symmetric(horizontal: 8),
       child: Column(
         children: <Widget>[
           Card(
--- lib/Screens/CuisineScreen.dart
+++ lib/Screens/CuisineScreen.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:feelsgood/Models/Cuisine.dart';
 import 'package:feelsgood/bloc/Cuisinebloc.dart';
 import 'package:feelsgood/Components/CuisineCard.dart';
+import 'package:feelsgood/bloc/Pcuisinebloc.dart';
+import 'package:feelsgood/Components/BottomBar.dart';
+import 'package:feelsgood/Models/Type.dart';
+import 'package:feelsgood/Components/PcuisineCard.dart';
 
 class CuisineScreen extends StatefulWidget {
   static const String id = '/Cuisine';
@@ -11,25 +15,28 @@ class CuisineScreen extends StatefulWidget {
 
 class _CuisineScreenState extends State<CuisineScreen> {
   final CuisineBloc _cuisineBloc = CuisineBloc();
+  final PcuisineBloc _pcuisineBloc = PcuisineBloc();
 
   @override
   void dispose() {
     super.dispose();
     _cuisineBloc.dispose();
+    _pcuisineBloc.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Color(0xFFE91212),
+        backgroundColor: Color(0xFF9D0707),
         elevation: 0,
         title: Text(
           "Browse By Cuisine",
           style: TextStyle(
-            color: Colors.white,
-            fontSize: 17,
-          ),
+              color: Colors.white,
+              fontSize: 18,
+              fontFamily: "Open Sans",
+              fontWeight: FontWeight.w900),
         ),
         leading: Padding(
           padding: const EdgeInsets.all(8.0),
@@ -50,30 +57,76 @@ class _CuisineScreenState extends State<CuisineScreen> {
           ),
         ],
       ),
-      body: Container(
-        height: 700,
-        color: Color(0xFFFD8E8E),
-        child: StreamBuilder<List<Cuisine>>(
-          stream: _cuisineBloc.cuisineListStream,
-          builder:
-              (BuildContext context, AsyncSnapshot<List<Cuisine>> snapshot) {
-            if (snapshot.hasError) {
-              print(snapshot.error);
-            }
-            return snapshot.hasData
-                ? ListView.builder(
-                    shrinkWrap: true,
-                    scrollDirection: Axis.vertical,
-                    itemCount: snapshot.data.length,
-                    itemBuilder: (context, index) {
-                      return CuisineCard(
-                        cuisine: snapshot.data[index],
-                      );
-                    })
-                : Center(
-                    child: CircularProgressIndicator(),
-                  );
-          },
+      bottomNavigationBar: BottomBar(),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Container(
+              height: 100,
+              color: Color(0xFF9D0707),
+              width: MediaQuery.of(context).size.width,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: <Widget>[
+                  Container(
+                    height: 85,
+                    width: MediaQuery.of(context).size.width,
+                    child: StreamBuilder<List<Type>>(
+                      stream: _pcuisineBloc.pcuisineListStream,
+                      builder: (BuildContext context,
+                          AsyncSnapshot<List<Type>> snapshot) {
+                        if (snapshot.hasError) {
+                          print(snapshot.error);
+                        }
+                        return snapshot.hasData
+                            ? ListView.builder(
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                itemCount: snapshot.data.length,
+                                itemBuilder: (context, index) {
+                                  return PcuisineCard(
+                                    cuisine: snapshot.data[index],
+                                  );
+                                })
+                            : Center(
+                                child: CircularProgressIndicator(),
+                              );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              height: 700,
+              color: Color(0xFFEFCDB1),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8),
+                child: StreamBuilder<List<Cuisine>>(
+                  stream: _cuisineBloc.cuisineListStream,
+                  builder: (BuildContext context,
+                      AsyncSnapshot<List<Cuisine>> snapshot) {
+                    if (snapshot.hasError) {
+                      print(snapshot.error);
+                    }
+                    return snapshot.hasData
+                        ? ListView.builder(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.vertical,
+                            itemCount: snapshot.data.length,
+                            itemBuilder: (context, index) {
+                              return CuisineCard(
+                                cuisine: snapshot.data[index],
+                              );
+                            })
+                        : Center(
+                            child: CircularProgressIndicator(),
+                          );
+                  },
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
