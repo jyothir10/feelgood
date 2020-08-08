4c4ef4889061a9c3f9b4541ab1c310045802f9ca
sanika2001 <porayilsanika@gmail.com>
2020-08-08 13:21:18 +0530

    drawer

---
+++ lib/Components/ChangeCard.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ChangeCard extends StatelessWidget {
+  final Color color;
+  final Function onPressed;
+  final String text;
+  final String image;
+
+  ChangeCard({this.text, this.image, this.color, this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 152,
+      child: RaisedButton(
+        onPressed: onPressed,
+        elevation: 20,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            Text(
+              text,
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Image.asset(
+              image,
+              height: 20,
+              width: 20,
+            ),
+          ],
+        ),
+        color: color,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+      ),
+    );
+  }
+}
